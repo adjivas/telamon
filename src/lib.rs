b5bf329344330dfe78efe480aeac8442f3842ad0
adjivas <adjivas@users.noreply.github.com>
2018-04-12 18:04:53 +0200

    next step, write a unit test

--- src/lib.rs
+++ src/lib.rs
@@ -3,37 +3,40 @@ extern crate libc;
 use std::mem;
 
 /// https://westes.github.io/flex/manual/About-yyscan_005ft.html
-type yyscan_t = libc::c_void;
-
+type yyscan_t = *mut libc::c_void;
 type yy_buffer_state = *mut libc::c_void;
-type YY_BUFFER_STATE = yy_buffer_state;
 
 extern {
-    pub static lines: libc::c_int;
+    static lines: libc::c_int;
 
-    fn yylex_init(scanner: *mut yyscan_t) -> libc::c_int;
-    fn yy_scan_string(yy_str: *const libc::c_char, yyscanner: *mut yyscan_t) -> YY_BUFFER_STATE;
-    fn yylex(yyscanner: *mut yyscan_t) -> libc::c_int;
-    fn yy_delete_buffer(b: YY_BUFFER_STATE, yyscanner: *mut yyscan_t);
-    fn yylex_destroy(yyscanner: *mut yyscan_t) -> libc::c_int;
+    fn poc_yylex_init(scanner: yyscan_t) -> libc::c_int;
+    fn yylex_init(scanner: yyscan_t) -> libc::c_int;
+    fn yy_scan_string(yy_str: *const libc::c_char, yyscanner: yyscan_t) -> yy_buffer_state;
+    fn yylex(yyscanner: yyscan_t) -> libc::c_int;
+    fn yy_delete_buffer(b: yy_buffer_state, yyscanner: yyscan_t);
+    fn yylex_destroy(yyscanner: yyscan_t) -> libc::c_int;
+
+    fn ff(input: *const libc::c_char /*, scanner: yyscan_t */);
 }
 
 pub struct Lexer {
-    scanner: *mut yyscan_t,
-    buffer: YY_BUFFER_STATE,
+//    scanner: yyscan_t,
+//    buffer: yy_buffer_state,
 }
 
 impl From<*const libc::c_char> for Lexer {
     /// https://westes.github.io/flex/manual/Init-and-Destroy-Functions.html#index-yylex_005finit
     fn from(input: *const libc::c_char) -> Self {
         unsafe {
-            let mut scanner: yyscan_t = mem::uninitialized();
+            let scanner: yyscan_t = mem::zeroed();
 
-            println!("{}",yylex_init(&mut scanner) );
+//            poc_yylex_init(scanner);
+            ff(input);
+//	    let buffer = mem::zeroed();
+//            let buffer = yy_scan_string(input, scanner);
             Lexer {
-                scanner: &mut scanner,
-                buffer: mem::uninitialized(),
-                //yy_scan_string(input, &mut scanner),
+//                scanner: scanner,
+//                buffer: buffer,
             }
         }
     }
@@ -43,24 +46,23 @@ impl Iterator for Lexer {
     type Item = libc::c_int;
 
     fn next(&mut self) -> Option<Self::Item> {
-
-        unsafe {
-        println!("ss");
-        let code = yylex(self.scanner);
-        println!("{:?}", code);
+	None
+        /*unsafe {
+            let code = yylex(self.scanner);
+            println!("{:?}", code);
             match code {
                 libc::EOF => None,
                 code => Some(code),
             }
-        }
+        }*/
     }
 }
 
 impl Drop for Lexer {
     fn drop(&mut self) {
-        unsafe {
-//          println!("{:?}",  yy_delete_buffer(self.buffer, self.scanner) );
-//         println!("{:?}",   yylex_destroy(self.scanner) );
-        }
+        /*unsafe {
+            yy_delete_buffer(self.buffer, self.scanner);
+            yylex_destroy(self.scanner);
+        }*/
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
 extern crate poc;
 
-use std::ffi::CString;
-use std::os::raw::c_char;
+use poc::Lexer;
 
 fn main() {
-        let raw = b"// coment \n other      \n \0".to_vec();
-        
-        unsafe {
-            let input = CString::from_vec_unchecked(raw);
-            let mut lex = poc::Lexer::from(input.as_ptr());
-
-            println!("{:?}", lex.next() );
-        }
+    let raw: Vec<u8> = b"// coment \n other      \n\0".to_vec();
+ 
+    let _: Lexer = Lexer::from(raw.as_ptr() as *const _);
 }
