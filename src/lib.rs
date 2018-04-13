3b70839e40b88ea340898246453a68c5f660dc7f
adjivas <adjivas@users.noreply.github.com>
2018-04-13 19:31:18 +0200

    fast save, resolution of mature ffi

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,42 @@
 extern crate libc;
 
 use std::mem;
+use std::ptr;
 
 /// https://westes.github.io/flex/manual/About-yyscan_005ft.html
-type yyscan_t = *mut libc::c_void;
-type yy_buffer_state = *mut libc::c_void;
+pub type yyscan_t = *const libc::c_void;
+pub type yy_buffer_state = *const libc::c_void;
 
 extern {
-    static lines: libc::c_int;
+    pub static lines: libc::c_int;
 
-    fn poc_yylex_init(scanner: yyscan_t) -> libc::c_int;
-    fn yylex_init(scanner: yyscan_t) -> libc::c_int;
-    fn yy_scan_string(yy_str: *const libc::c_char, yyscanner: yyscan_t) -> yy_buffer_state;
-    fn yylex(yyscanner: yyscan_t) -> libc::c_int;
-    fn yy_delete_buffer(b: yy_buffer_state, yyscanner: yyscan_t);
-    fn yylex_destroy(yyscanner: yyscan_t) -> libc::c_int;
+    pub fn yylex_init(scanner: *const yyscan_t) -> libc::c_int;
+    pub fn yy_scan_string(yy_str: *const libc::c_char, yyscanner: yyscan_t) -> yy_buffer_state;
+    pub fn yylex(yyscanner: yyscan_t) -> libc::c_int;
+    pub fn yyget_text(yyscanner: yyscan_t) -> *const libc::c_char;
+    pub fn yy_delete_buffer(b: yy_buffer_state, yyscanner: yyscan_t);
+    pub fn yylex_destroy(yyscanner: yyscan_t) -> libc::c_int;
 
-    fn ff(input: *const libc::c_char /*, scanner: yyscan_t */);
+    pub static poc_input: *const libc::c_char;
+    pub static poc_scanner: yyscan_t;
+    pub static poc_buffer_state: yy_buffer_state;
 }
 
 pub struct Lexer {
-//    scanner: yyscan_t,
-//    buffer: yy_buffer_state,
+    scanner: yyscan_t,
+    buffer: yy_buffer_state,
 }
 
 impl From<*const libc::c_char> for Lexer {
     /// https://westes.github.io/flex/manual/Init-and-Destroy-Functions.html#index-yylex_005finit
     fn from(input: *const libc::c_char) -> Self {
         unsafe {
-            let scanner: yyscan_t = mem::zeroed();
+            let scanner: yyscan_t = ptr::null();
 
-//            poc_yylex_init(scanner);
-            ff(input);
-//	    let buffer = mem::zeroed();
-//            let buffer = yy_scan_string(input, scanner);
+            yylex_init(&scanner);
             Lexer {
-//                scanner: scanner,
-//                buffer: buffer,
+                scanner: scanner,
+                buffer: yy_scan_string(input, scanner),
             }
         }
     }
@@ -46,23 +46,25 @@ impl Iterator for Lexer {
     type Item = libc::c_int;
 
     fn next(&mut self) -> Option<Self::Item> {
-	None
-        /*unsafe {
-            let code = yylex(self.scanner);
-            println!("{:?}", code);
-            match code {
-                libc::EOF => None,
-                code => Some(code),
-            }
-        }*/
+        unsafe {
+            match yylex(poc_scanner) {
+		libc::EOF => None,
+                code => {
+			println!("lines: {}, code: {}", lines, code);
+			let out: *const libc::c_char = yyget_text(self.scanner);
+			libc::write(1, out as *const _, libc::strlen(out as *const _));
+			Some(code)
+                },
+           }
+        }
     }
 }
 
 impl Drop for Lexer {
     fn drop(&mut self) {
-        /*unsafe {
-            yy_delete_buffer(self.buffer, self.scanner);
-            yylex_destroy(self.scanner);
-        }*/
+	unsafe {
+	        yy_delete_buffer(self.buffer, self.scanner);
+        	yylex_destroy(self.scanner);
+	}
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
 extern crate poc;
+extern crate libc;
 
-use poc::Lexer;
+//use poc::Lexer;
+use std::{mem,ptr};
 
 fn main() {
-    let raw: Vec<u8> = b"// coment \n other      \n\0".to_vec();
- 
-    let _: Lexer = Lexer::from(raw.as_ptr() as *const _);
+    unsafe {
+        let poc_input: *const libc::c_char = b"/// ll \n ll \0".as_ptr() as *const _;
+        let poc_scanner: poc::yyscan_t = ptr::null();
+
+        poc::yylex_init(&poc_scanner);
+
+        let poc_buffer_state: poc::yy_buffer_state = poc::yy_scan_string(poc_input, poc_scanner);
+        loop {
+            match poc::yylex(poc_scanner) {
+		libc::EOF => break ,
+                code => {
+			println!("lines: {}, code: {}", poc::lines, code);
+			let out: *const libc::c_char = poc::yyget_text(poc_scanner);
+			libc::write(1, out as *const _, libc::strlen(out as *const _));
+                },
+            }
+        }
+	println!();
+        poc::yy_delete_buffer(poc_buffer_state, poc_scanner);
+        poc::yylex_destroy(poc_scanner);
+    }
 }
