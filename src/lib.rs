baf3eb15a95468664a35842785bd8844bc10fff2
adjivas <adjivas@users.noreply.github.com>
2018-04-15 13:40:14 +0000

    ffi has Rust safe interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,23 @@
 extern crate libc;
 
-use std::mem;
+mod ffi;
+
 use std::ptr;
 
-/// https://westes.github.io/flex/manual/About-yyscan_005ft.html
-pub type yyscan_t = *const libc::c_void;
-pub type yy_buffer_state = *const libc::c_void;
-
-extern {
-    pub static lines: libc::c_int;
-
-    pub fn yylex_init(scanner: *const yyscan_t) -> libc::c_int;
-    pub fn yy_scan_string(yy_str: *const libc::c_char, yyscanner: yyscan_t) -> yy_buffer_state;
-    pub fn yylex(yyscanner: yyscan_t) -> libc::c_int;
-    pub fn yyget_text(yyscanner: yyscan_t) -> *const libc::c_char;
-    pub fn yy_delete_buffer(b: yy_buffer_state, yyscanner: yyscan_t);
-    pub fn yylex_destroy(yyscanner: yyscan_t) -> libc::c_int;
-
-    pub static poc_input: *const libc::c_char;
-    pub static poc_scanner: yyscan_t;
-    pub static poc_buffer_state: yy_buffer_state;
-}
-
 pub struct Lexer {
-    scanner: yyscan_t,
-    buffer: yy_buffer_state,
+    scanner: ffi::YyScan,
+    buffer: ffi::YyBufferState,
 }
 
 impl From<*const libc::c_char> for Lexer {
-    /// https://westes.github.io/flex/manual/Init-and-Destroy-Functions.html#index-yylex_005finit
     fn from(input: *const libc::c_char) -> Self {
         unsafe {
-            let scanner: yyscan_t = ptr::null();
+            let scanner: ffi::YyScan = ptr::null();
 
-            yylex_init(&scanner);
+            ffi::yylex_init(&scanner); // https://westes.github.io/flex/manual/Init-and-Destroy-Functions.html#index-yylex_005finit
             Lexer {
                 scanner: scanner,
-                buffer: yy_scan_string(input, scanner),
+                buffer: ffi::yy_scan_string(input, scanner), // https://westes.github.io/flex/manual/Multiple-Input-Buffers.html
             }
         }
     }
@@ -47,24 +28,25 @@ impl Iterator for Lexer {
 
     fn next(&mut self) -> Option<Self::Item> {
         unsafe {
-            match yylex(poc_scanner) {
-		libc::EOF => None,
+            match ffi::yylex(self.scanner) {
+                libc::EOF => None,
                 code => {
-			println!("lines: {}, code: {}", lines, code);
-			let out: *const libc::c_char = yyget_text(self.scanner);
-			libc::write(1, out as *const _, libc::strlen(out as *const _));
-			Some(code)
+                    let out: *const libc::c_char = ffi::yyget_text(self.scanner);
+
+	                libc::write(1, out as *const _, libc::strlen(out as *const _));
+                    Some(code)
                 },
-           }
+            }
         }
     }
 }
 
 impl Drop for Lexer {
     fn drop(&mut self) {
-	unsafe {
-	        yy_delete_buffer(self.buffer, self.scanner);
-        	yylex_destroy(self.scanner);
-	}
+        unsafe {
+	        libc::write(1, b"\n".as_ptr() as *const _, 1);
+            ffi::yy_delete_buffer(self.buffer, self.scanner); // https://westes.github.io/flex/manual/Multiple-Input-Buffers.html
+            ffi::yylex_destroy(self.scanner); // https://westes.github.io/flex/manual/Init-and-Destroy-Functions.html#index-yylex_005finit
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,12 @@
 extern crate poc;
 extern crate libc;
 
-//use poc::Lexer;
-use std::{mem,ptr};
+use poc::Lexer;
 
 fn main() {
-    unsafe {
-        let poc_input: *const libc::c_char = b"/// ll \n ll \0".as_ptr() as *const _;
-        let poc_scanner: poc::yyscan_t = ptr::null();
+    let poc_input: *const libc::c_char = b"/// ll \n ll \0".as_ptr() as *const _;
+    let mut lexer = Lexer::from(poc_input);
 
-        poc::yylex_init(&poc_scanner);
-
-        let poc_buffer_state: poc::yy_buffer_state = poc::yy_scan_string(poc_input, poc_scanner);
-        loop {
-            match poc::yylex(poc_scanner) {
-		libc::EOF => break ,
-                code => {
-			println!("lines: {}, code: {}", poc::lines, code);
-			let out: *const libc::c_char = poc::yyget_text(poc_scanner);
-			libc::write(1, out as *const _, libc::strlen(out as *const _));
-                },
-            }
-        }
-	println!();
-        poc::yy_delete_buffer(poc_buffer_state, poc_scanner);
-        poc::yylex_destroy(poc_scanner);
+    while let Some(_) = lexer.next() {
     }
 }
